3cc7c2f9ca771aa2074da30fd5e84820e850be1e
Siddiqur Rahman <sm.siddiq79@gmail.com>
2019-10-15 15:06:04 +0600

    update scroll nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,18 +5,19 @@
             $(".nav-wrapper").slideToggle();
         });
 
-        $('.scrollDown').on('click', function () {
-            var id = $(this).attr('href');
-            if ($(window).width() > 767) {
-                $('html, body').stop(true, true).animate({
-                    scrollTop: $(id).offset().top - 0
-                }, 900);
-            } else {
-                $('html, body').stop(true, true).animate({
-                    scrollTop: $(id).offset().top - 91
-                }, 900);
+        $('a[href*="#"]:not([href="#"])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
             }
         });
+        
     }); // End ready function
 
     $(window).on('load', function () {
